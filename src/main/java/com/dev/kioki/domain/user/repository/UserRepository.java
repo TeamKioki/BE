12fb291a163a934f1a69de8844fc8d36b9b649fc
yewonahn <yewonahn@naver.com>
2024-08-09 12:19:47 +0900

    feat: userDetails 구현 (#20)

--- src/main/java/com/dev/kioki/domain/user/repository/UserRepository.java
+++ src/main/java/com/dev/kioki/domain/user/repository/UserRepository.java
@@ -3,5 +3,10 @@ package com.dev.kioki.domain.user.repository;
 import com.dev.kioki.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Override
+    Optional<User> findByPhone(String phone);
 }
