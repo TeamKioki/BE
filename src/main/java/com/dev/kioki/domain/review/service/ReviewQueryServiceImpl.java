0b1576761615cf8cc7b5bdc6971f0aa4861dd66e
yeessonng <dorlek856@gmail.com>
2024-08-23 01:57:52 +0900

    feat: 리뷰 추가

--- src/main/java/com/dev/kioki/domain/review/service/ReviewQueryServiceImpl.java
+++ src/main/java/com/dev/kioki/domain/review/service/ReviewQueryServiceImpl.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 @Transactional
 public class ReviewQueryServiceImpl implements ReviewQueryService {
-    private final ReviewRepository reviewRepository;
 
 
 }
